596857ee2861f45e315d5d23368d10dfae255a29
sagardas <sagardas.195@gmail.com>
2023-02-15 11:03:51 +0530

    DestinationCity

--- src/DestinationCity.java
+++ src/DestinationCity.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 class DestinationCity {
     public String destCity(List<List<String>> paths) {
-        String start=paths.get(0).get(0);
-        Map<String,Integer> map=new HashMap<>();
-        for(List<String> i:paths){
-            for(String s:i){
-                if(map.containsKey(s)){
-                    map.put(s,map.get(s)+1);
+        String start = paths.get(0).get(0);
+        Map<String, Integer> map = new HashMap<>();
+        for (List<String> i : paths) {
+            for (String s : i) {
+                if (map.containsKey(s)) {
+                    map.put(s, map.get(s) + 1);
 
-                }else{
-                    map.put(s,1);
+                } else {
+                    map.put(s, 1);
                 }
             }
         }
 
-        for(Map.Entry<String,Integer> en:map.entrySet()){
-            if(en.getValue()==1 && !en.getKey().equals(start)){
+        for (Map.Entry<String, Integer> en : map.entrySet()) {
+            if (en.getValue() == 1 && !en.getKey().equals(start)) {
                 return en.getKey();
 
             }
