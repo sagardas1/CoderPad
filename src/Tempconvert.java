393fc1d8aebd3871506ad97ae6c047da6dfaea9e
sagardas <sagardas.195@gmail.com>
2023-02-16 10:47:08 +0530

    refactored

--- src/Tempconvert.java
+++ src/Tempconvert.java
@@ -1,7 +1,7 @@
 public class Tempconvert {
 
     public double[] convertTemperature(double celsius) {
-        double i[]={celsius+273.15,(celsius*1.80+32.00)};
+        double i[] = {celsius + 273.15, (celsius * 1.80 + 32.00)};
         return i;
     }
 }
