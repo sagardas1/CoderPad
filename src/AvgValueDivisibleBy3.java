9a09b33c9de24809aaf494645dfeea108ec204fa
sagardas <sagardas.195@gmail.com>
2023-02-17 10:04:53 +0530

    smallesteven multiples

--- src/AvgValueDivisibleBy3.java
+++ src/AvgValueDivisibleBy3.java
@@ -1,24 +1,26 @@
 public class AvgValueDivisibleBy3 {
 
     public static void main(String[] args) {
-        int[] i={1,2,4,7,10};
+        int[] i = {1, 2, 4, 7, 10};
         System.out.println(averageValue(i));
     }
 
     public static int averageValue(int[] nums) {
-        int count=0;
-        int sum=0;
+        int count = 0;
+        int sum = 0;
 
 
-        for(int i:nums){
-            if(i%2==0 && i%3==0){
-                count+=1;
-                sum+=i;
+        for (int i : nums) {
+            if (i % 2 == 0 && i % 3 == 0) {
+                count += 1;
+                sum += i;
             }
         }
-        if(count==0){return 0;}
+        if (count == 0) {
+            return 0;
+        }
 
-        return sum/count;
+        return sum / count;
 
 
     }
