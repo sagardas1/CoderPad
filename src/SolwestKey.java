ce7ac731f9eea45eba0b6de31f0303747e5bc3ba
sagardas <sagardas.195@gmail.com>
2023-02-18 13:39:38 +0530

    SlowestKey

--- src/SolwestKey.java
+++ src/SolwestKey.java
@@ -1,15 +1,15 @@
 public class SolwestKey {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
-        int index=0;
-        int great=releaseTimes[0];
-        for(int i=1;i<releaseTimes.length;i++){
-            if(releaseTimes[i]-releaseTimes[i-1]>great){
-                great=releaseTimes[i]-releaseTimes[i-1];
-                index=i;
-            }else if(releaseTimes[i]-releaseTimes[i-1]==great){
-                great=releaseTimes[i]-releaseTimes[i-1];
-                if(keysPressed.charAt(index)<keysPressed.charAt(i)){
-                    index=i;
+        int index = 0;
+        int great = releaseTimes[0];
+        for (int i = 1; i < releaseTimes.length; i++) {
+            if (releaseTimes[i] - releaseTimes[i - 1] > great) {
+                great = releaseTimes[i] - releaseTimes[i - 1];
+                index = i;
+            } else if (releaseTimes[i] - releaseTimes[i - 1] == great) {
+                great = releaseTimes[i] - releaseTimes[i - 1];
+                if (keysPressed.charAt(index) < keysPressed.charAt(i)) {
+                    index = i;
                 }
             }
         }
