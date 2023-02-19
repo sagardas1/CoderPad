4908617d4613600108f4ae2b92497f730158ab8a
sagardas <sagardas.195@gmail.com>
2023-02-19 15:59:22 +0530

    Refactored

--- src/Avoid1576.java
+++ src/Avoid1576.java
@@ -1,30 +1,32 @@
 public class Avoid1576 {
     public static String modifyString(String s) {
-        String k="";
-        char c='a';
-        char arr[]=s.toCharArray();
-        if(s.length()==1 && s.charAt(0)=='?'  ){return "a";}
-        if(s.length()==1 &&!s.contains("?"))return s;
-        for(int i=0;i<arr.length;i++){
-            if(i==0 && arr[i]=='?'){
-                while(arr[i+1]==c){
-                    c+=1;
+        String k = "";
+        char c = 'a';
+        char arr[] = s.toCharArray();
+        if (s.length() == 1 && s.charAt(0) == '?') {
+            return "a";
+        }
+        if (s.length() == 1 && !s.contains("?")) return s;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0 && arr[i] == '?') {
+                while (arr[i + 1] == c) {
+                    c += 1;
                 }
-                arr[i]=c;
-                c='a';
+                arr[i] = c;
+                c = 'a';
 
-            }else{
-                if(arr[i]=='?') {
-                    while (arr[i]==c || arr[i + 1]==c) {
+            } else {
+                if (arr[i] == '?') {
+                    while (arr[i] == c || arr[i + 1] == c) {
                         c += 1;
                     }
-                    arr[i] = c ;
+                    arr[i] = c;
                     c = 'a';
                 }
             }
 
 
         }
-        return  String.valueOf(arr);
+        return String.valueOf(arr);
     }
 }
