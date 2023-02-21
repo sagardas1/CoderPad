71d37f000a64f6ef80b69ac0e92febbf5dd0628f
sagardas <sagardas.195@gmail.com>
2023-02-21 20:40:40 +0530

    String Percentage

--- src/StringPercentage.java
+++ src/StringPercentage.java
@@ -1,9 +1,9 @@
 public class StringPercentage {
     public static int percentageLetter(String s, char letter) {
-        int count=0;
-        for(char c:s.toCharArray()){
-            if(c==letter)count+=1;
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == letter) count += 1;
         }
-        return (count*100)/s.length();
+        return (count * 100) / s.length();
     }
 }
