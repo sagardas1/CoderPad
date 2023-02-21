99949912f482b60c0217c68f8c9ac52ff8d6e7f0
sagardas <sagardas.195@gmail.com>
2023-02-21 20:41:53 +0530

    Decode string

---
+++ src/DecodeMessage.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class DecodeMessage {
+
+    public static String decodeMessage(String key, String message) {
+        char c='a';
+        key=key.replaceAll(" ","");
+        key=key.trim();
+
+        Set<Character> set=new HashSet<>();
+
+
+
+        Map<Character,Character> map=new LinkedHashMap<>();
+        for(char i:key.toCharArray()){
+            if(i!=' ' &&set.add(i)){
+                map.put(i,c);
+                c+=1;
+            }
+        }
+        System.out.println(map);
+
+
+        String w="";
+        for(char j:message.toCharArray()){
+            if(j!=' ')
+                w+=map.get(j)+"";
+            else w+=" ";
+        }
+
+
+        return w;
+    }
+}
