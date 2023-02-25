591870bafa8f877f532784772835d5f9891e6857
sagardas <sagardas.195@gmail.com>
2023-02-25 13:21:40 +0530

    string allmost equal

--- src/MaxLenSubArray.java
+++ src/MaxLenSubArray.java
@@ -8,7 +8,7 @@ public class MaxLenSubArray {
     public static int maximumWealth(int[][] accounts) {
         int sum = 0;
         int num = 0;
-        System.out.println(accounts.length);
+
         for (int i = 0; i < accounts.length; i++) {
             num = 0;
             for (int j = 0; j < accounts[0].length; j++) {
