23a435aa918ccba6d6f8e9cb260789e9f2c46c2d
sagardas <sagardas.195@gmail.com>
2023-02-26 15:22:38 +0530

    Circular string

--- src/CircularString.java
+++ src/CircularString.java
@@ -2,14 +2,19 @@ public class CircularString {
     public static void main(String[] args) {
 
     }
-    public boolean isCircularSentence(String sentence) {
-String arr[]=sentence.split(" ");
-if(arr[0].charAt(0)==arr[arr.length-1].charAt((arr[arr.length-1]).length()-1)){
 
-    for(int i=1;i<arr.length;i++){
-        if(arr[i-1].charAt(arr[i-1].length()-1)==arr[i].charAt(0)){continue;}else{return false;}
-    }
-}else return false;
-return true;
+    public boolean isCircularSentence(String sentence) {
+        String arr[] = sentence.split(" ");
+        if (arr[0].charAt(0) == arr[arr.length - 1].charAt((arr[arr.length - 1]).length() - 1)) {
+
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i - 1].charAt(arr[i - 1].length() - 1) == arr[i].charAt(0)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        } else return false;
+        return true;
     }
 }
