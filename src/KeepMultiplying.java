32be0ff4ee13bfc2b21b93f03088def6716d663b
sagardas <sagardas.195@gmail.com>
2023-03-02 13:57:10 +0530

    KeepMultiplying

--- src/KeepMultiplying.java
+++ src/KeepMultiplying.java
@@ -17,4 +17,22 @@ public class KeepMultiplying {
 
             return k;
         }
+
+
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+            int count=0;
+
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                for (int k=0;k<arr.length;k++){
+if(i>=0&&j>i&& k>j && Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b && Math.abs(arr[i]-arr[k])<=c){
+    count+=1;
+}
+                }
+
+            }
+        }
+        return count;
+    }
 }
