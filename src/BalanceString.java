2aef48ad34c1c2184a33b23d969d1967c6139801
sagardas <sagardas.195@gmail.com>
2023-03-04 15:19:01 +0530

    KeepMultiplying

--- src/BalanceString.java
+++ src/BalanceString.java
@@ -1,49 +1,51 @@
 public class BalanceString {
     public static int minimumDeletions(String s) {
 
-        char arr[]=s.toCharArray();
-int count=0;
-boolean flag=false;
-if(s.replaceAll("a","").length()==0 || s.replaceAll("b","").length()==0) return 0;
-for(int i=0;i<arr.length;i++){
-    flag=false;
-    for(int j=i+1;j<arr.length;j++) {
-        if (arr[i]=='b' && arr[j]=='a') {
-           flag=true;
+        char arr[] = s.toCharArray();
+        int count = 0;
+        boolean flag = false;
+        if (s.replaceAll("a", "").length() == 0 || s.replaceAll("b", "").length() == 0) return 0;
+        for (int i = 0; i < arr.length; i++) {
+            flag = false;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == 'b' && arr[j] == 'a') {
+                    flag = true;
+                }
+            }
+            if (flag) {
+                count += 1;
+            }
         }
-    }
-    if(flag){count+=1;}
-}
 
-return count;
+        return count;
     }
 
     public static void main(String[] args) {
         System.out.println(minimumDeletions("aababbab"));
     }
 
-    public static  String reformatNumber(String number) {
-number=number.replaceAll(" ","");
-number=number.replaceAll("-","");
+    public static String reformatNumber(String number) {
+        number = number.replaceAll(" ", "");
+        number = number.replaceAll("-", "");
 
 
-int value=number.length()/3;
+        int value = number.length() / 3;
 
-        String s="";
-        int count=0;
+        String s = "";
+        int count = 0;
 
-while(value!=0){
+        while (value != 0) {ß
 
-    for(char c:number.toCharArray()){
-        s+=c;
-        count+=1;
-        if(count==3){
-            s+='-';
-            count=0;
-            value-=1;
+            for (char c : number.toCharArray()) {
+                s += c;
+                count += 1;
+                if (count == 3) {
+                    s += '-';
+                    count = 0;
+                    value -= 1;
+                }
+            }
         }
-    }
-}
-return "";
+        return "";
     }
 }
