fcfaebd569d3730430d46808c412d4a02f1521e5
sagardas <sagardas.195@gmail.com>
2023-03-08 10:16:58 +0530

    Day in a year

--- src/DayinYear.java
+++ src/DayinYear.java
@@ -2,45 +2,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DayinYear {
-    public static  int dayOfYear(String date) {
-        Map<Integer,Integer> map=new HashMap<>();
+    public static int dayOfYear(String date) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-        map.put(1,31);
-        map.put(2,28);
-        map.put(3,31);
-        map.put(4,30);
-        map.put(5,31);
-        map.put(6,30);
-        map.put(7,31);
-        map.put(8,31);
-        map.put(9,30);
-        map.put(10,31);
-        map.put(11,30);
-        map.put(12,31);
+        map.put(1, 31);
+        map.put(2, 28);
+        map.put(3, 31);
+        map.put(4, 30);
+        map.put(5, 31);
+        map.put(6, 30);
+        map.put(7, 31);
+        map.put(8, 31);
+        map.put(9, 30);
+        map.put(10, 31);
+        map.put(11, 30);
+        map.put(12, 31);
 
 
-        String arr[]=date.split( "-");
-        boolean flag=isLeap(Integer.valueOf(arr[0]));
-        int days=0;
-        for(int i=1;i<Integer.valueOf(arr[1]);i++){
-            if(i==2 ){
-                if(flag)
-                    days+=29;
-                else days+=28;
-            }else{
-                days+=map.get(i);
+        String arr[] = date.split("-");
+        boolean flag = isLeap(Integer.valueOf(arr[0]));
+        int days = 0;
+        for (int i = 1; i < Integer.valueOf(arr[1]); i++) {
+            if (i == 2) {
+                if (flag)
+                    days += 29;
+                else days += 28;
+            } else {
+                days += map.get(i);
             }
-
 
 
         }
 
-        days+=Integer.valueOf(arr[2]);
+        days += Integer.valueOf(arr[2]);
         return days;
 
     }
 
-    public static boolean isLeap(int year){
-        return (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0));
+    public static boolean isLeap(int year) {
+        return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
     }
 }
