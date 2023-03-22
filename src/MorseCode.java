4124779e883eeacf08b3ccf9820a3b446f44b292
sagardas <sagardas.195@gmail.com>
2023-03-22 12:45:12 +0530

    Day in a year

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -3,22 +3,22 @@ import java.util.Set;
 
 public class MorseCode {
     public static int uniqueMorseRepresentations(String[] words) {
-        Set<String> set=new HashSet<>();
-        String k[]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        String l="";
+        Set<String> set = new HashSet<>();
+        String k[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        String l = "";
 
-        int i=0;
-        for(char c='a';c<='z';c++){
-            l+=c+"";
+        int i = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            l += c + "";
         }
 
-        for(String s:words){
-            System.out.println("s-->>"+s);
-            String ll="";
-            for(String kk:s.split("")){
-                System.out.println("kk---->>"+kk);
+        for (String s : words) {
+            System.out.println("s-->>" + s);
+            String ll = "";
+            for (String kk : s.split("")) {
+                System.out.println("kk---->>" + kk);
 
-                ll+=k[l.indexOf(kk)];
+                ll += k[l.indexOf(kk)];
             }
             set.add(ll);
 
