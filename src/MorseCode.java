97636377f9561785db82a85f7afa922c8820fd5e
sagardas <sagardas.195@gmail.com>
2023-03-22 12:44:52 +0530

    Day in a year

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -1,2 +1,25 @@
 public class MorseCode {
+    public static int uniqueMorseRepresentations(String[] words) {
+        Set<String> set=new HashSet<>();
+        String k[]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String l="";
+
+        int i=0;
+        for(char c='a';c<='z';c++){
+            l+=c+"";
+        }
+
+        for(String s:words){
+            System.out.println("s-->>"+s);
+            String ll="";
+            for(String kk:s.split("")){
+                System.out.println("kk---->>"+kk);
+
+                ll+=k[l.indexOf(kk)];
+            }
+            set.add(ll);
+
+        }
+        return set.size();
+    }
 }
