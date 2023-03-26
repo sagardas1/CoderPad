479adeca96c564de6c3dd9782cd1384b8de7b4f7
sagardas <sagardas.195@gmail.com>
2023-03-26 13:09:35 +0530

    Day in a year

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -53,10 +53,23 @@ public class MorseCode {
     }
 
     public static void main(String[] args) {
-        System.out.println(passThePillow(18, 38));
+        int a[]={1,2,3,4};
+        System.out.println(countPairs(a, 1));
     }
 
+    public static int countPairs(int[] nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j] && (i*j)%k==0){
+                    count+=1;
+                }
+            }
 
+        }
+        if(count==0) return 0;
+        return count;
+    }
     public static int balancedStringSplit(String s) {
         int r = 0;
         int l = 0;
