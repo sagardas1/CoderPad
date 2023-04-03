265d3b3edc619242e04d7fb12a026e4d10a53429
sagardas <sagardas.195@gmail.com>
2023-04-03 19:44:56 +0530

    Added imports

--- src/TwoOutOfThree.java
+++ src/TwoOutOfThree.java
@@ -1,38 +1,43 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class TwoOutOfThree {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
-        List<Integer> l1=Arrays.stream(nums1)
+        List<Integer> l1 = Arrays.stream(nums1)
                 .boxed()
                 .collect(Collectors.toList());
-        List<Integer> l2=Arrays.stream(nums2)
+        List<Integer> l2 = Arrays.stream(nums2)
                 .boxed()
                 .collect(Collectors.toList());
-        List<Integer> l3=Arrays.stream(nums3)
+        List<Integer> l3 = Arrays.stream(nums3)
                 .boxed()
                 .collect(Collectors.toList());
 
-        Set<Integer> set=new HashSet<>();
-        List<Integer> finalCount =new ArrayList<>();
-        for(int i:nums1){
+        Set<Integer> set = new HashSet<>();
+        List<Integer> finalCount = new ArrayList<>();
+        for (int i : nums1) {
             set.add(i);
         }
-        for(int i:nums2){
+        for (int i : nums2) {
             set.add(i);
         }
-        for(int i:nums3){
+        for (int i : nums3) {
             set.add(i);
         }
-        for(int i:set){
-            int count=0;
-            if(l1.contains((Object)i)){
-                count+=1;
+        for (int i : set) {
+            int count = 0;
+            if (l1.contains((Object) i)) {
+                count += 1;
             }
-            if(l2.contains((Object)i)){
-                count+=1;
+            if (l2.contains((Object) i)) {
+                count += 1;
             }
-            if(l3.contains((Object)i)){
-                count+=1;
+            if (l3.contains((Object) i)) {
+                count += 1;
             }
-            if(count>=2){
+            if (count >= 2) {
                 finalCount.add(i);
             }
         }
