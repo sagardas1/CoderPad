9764303dae56c07bf5d72597aa3a405b13ef5274
sagardas <sagardas.195@gmail.com>
2023-06-13 22:19:32 +0530

    spaces

--- src/AlternateDigitSum.java
+++ src/AlternateDigitSum.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class AlternateDigitSum {
     public int alternateDigitSum(int n) {
         if(n<10){return n;}
@@ -13,6 +17,26 @@ public class AlternateDigitSum {
             }
         }
         return k;
+
+    }
+
+
+
+    public static void main(String[] args) {
+Set<Integer> n=Set.of(2,4,6,8);
+n.stream().takeWhile(i->i%2==0).forEach(System.out::println);
+
+    }
+
+    public int h(int i, int j){
+        return i+j;
+    }
+}
+interface Card {
+    private Long createCardId(){
+       return 0L;
+    }
+    private static void displayCardDetails(){
 
     }
 }
